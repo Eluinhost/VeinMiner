236cbedb0564533342c518fbd392a65d231920d4
Eluinhost <graham_howden1@yahoo.co.uk>
2016-06-03 18:00:58 +0100

    deal with redstone/glowing redstone ore

--- src/main/kotlin/gg/uhc/veinminer/VeinTraverser.kt
+++ src/main/kotlin/gg/uhc/veinminer/VeinTraverser.kt
@@ -1,11 +1,17 @@
 package gg.uhc.veinminer
 
 import com.google.common.collect.Lists
+import org.bukkit.Material
 import org.bukkit.block.Block
 
 class VeinTraverser(val maxSize: Int) {
+    fun convertType(type: Material) = when(type) {
+        Material.GLOWING_REDSTONE_ORE -> Material.REDSTONE_ORE
+        else -> type
+    }
+
     fun getMatchingTypesAtLocation(block: Block): Set<Block> {
-        val type = block.type
+        val type = convertType(block.type)
 
         val matching = mutableSetOf(block)
 
@@ -28,7 +34,7 @@ class VeinTraverser(val maxSize: Int) {
                         check = current.getRelative(x, y, z)
 
                         // skip outside world and wrong types
-                        if (check == null || check.type != type) continue
+                        if (check == null || type != convertType(check.type)) continue
 
                         // add to matching set
                         if (matching.add(check)) {
